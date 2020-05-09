f70274c7a04dbf71bbbff78b05202873b5a4f6bb
Longafter <zhangpingzhao_cs@163.com>
2020-05-10 00:48:12 +0800

    admin定制

--- lefthand/blog/admin.py
+++ lefthand/blog/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
+from django.contrib.admin.models import LogEntry
 from django.urls import reverse
 from django.utils.html import format_html
 
 from .models import Category, Tag, Post
+from .adminforms import PostAdminForm
+
+from lefthand.base_admin import BaseOwnerAdmin
+from lefthand.custom_site import custom_site
 
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count')
-    fields = ('name', 'status', 'is_nav', 'owner')
-
-    # 将 owner 字段设定为当前的登录用户
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super().save_model(request, obj, form, change)
+@admin.register(Category, site=custom_site)
+class CategoryAdmin(BaseOwnerAdmin):
+    list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count', 'owner')
+    fields = ('name', 'status', 'is_nav')
 
     # 显示分类下的文章数量
     def post_count(self, obj):
@@ -21,35 +21,59 @@ class CategoryAdmin(admin.ModelAdmin):
     post_count.short_description = "文章数量"
 
 
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'created_time')
-    fields = ('name', 'status', 'owner')
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """ 自定义过滤器只展示当前用户分类 """
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
 
-    # 将 owner 字段设定为当前的登录用户
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super().save_model(request, obj, form, change)
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
 
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+@admin.register(Tag, site=custom_site)
+class TagAdmin(BaseOwnerAdmin):
+    list_display = ('name', 'status', 'created_time', 'owner')
+    fields = ('name', 'status')
+
+
+@admin.register(Post, site=custom_site)
+class PostAdmin(BaseOwnerAdmin):
+    form = PostAdminForm
+
     list_display = [
         'title', 'category', 'status',
-        'created_time', 'operator',
+        'created_time', 'owner', 'operator',
     ]
-    fields = (
-        ('category', 'title'),
-        'desc',
-        'status',
-        'content',
-        'tag',
-        'created_time',
+    exclude = ('owner', )
+    fieldsets = (
+        ('基础配置', {
+            'fields': (
+                ('title', 'category'),
+                'status',
+                'created_time',
+            ),
+        }),
+        ('内容', {
+            'fields': (
+                'desc',
+                'content',
+            ),
+        }),
+        ('额外信息', {
+            'classes': ('wide', ),
+            'fields': ('tag', ),
+        })
     )
     # 配置哪些字段可以作为链接
     list_display_links = []
     # 配置页面过滤器，需要通过哪些字段来过滤列表页
-    list_filter = ['category', ]
+    list_filter = [CategoryOwnerFilter]
     # 配置搜索字段 
     search_fields = ['title', 'category__name']
     # 动作相关的配置，是否展示在顶部
@@ -58,15 +82,21 @@ class PostAdmin(admin.ModelAdmin):
     actions_on_bottom = True
     # 保存、编辑、编辑并新建按钮是否在顶部展示 
     save_on_top = True
+    # 横向展示字段
+    filter_horizontal = ('tag', )
+    # 纵向展示字段
+    filter_vertical = ()
 
     # 展示自定义宇段
     def operator(self, obj):
         return format_html(
             '<a href="{}">编辑</a>',
-            reverse('admin:blog_post_change', args=(obj.id, ))
+            reverse('cus_admin:blog_post_change', args=(obj.id, ))
         )
     operator.short_description = '操作'
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super().save_model(request, obj, form, change)
+
+@admin.register(LogEntry, site=custom_site)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ['object_repr', 'object_id',
+        'action_flag', 'user', 'change_message']
---
+++ lefthand/lefthand/base_admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+
+class BaseOwnerAdmin(admin.ModelAdmin):
+    """
+        1. 自动补充分类，标签，文章，侧边栏，友链这些Model的Owner字段
+        2. 针对 queryset 过滤当前用户数据
+    """
+    exclude = ('owner', )
+
+    def save_model(self, request, obj, form, change):
+        return super().save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(owner=request.user)
